9d9cde1355ff4464ec4090aef9c5f8b4d10b8441
Derek Sauer <dereksauer.ca@gmail.com>
2025-02-17 11:38:09 -0500

    Integrate debugging adapter.
    Signed-off-by: Derek Sauer <dereksauer.ca@gmail.com>

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,181 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        keys = {
+            {
+                "<leader>dD",
+                function()
+                    require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+                end,
+                desc = "Breakpoint Condition",
+            },
+            {
+                "<leader>dd",
+                function()
+                    require("dap").toggle_breakpoint()
+                end,
+                desc = "Toggle Breakpoint",
+            },
+            {
+                "<leader>dc",
+                function()
+                    require("dap").continue()
+                end,
+                desc = "Run/Continue",
+            },
+            {
+                "<leader>dC",
+                function()
+                    require("dap").run_to_cursor()
+                end,
+                desc = "Run to Cursor",
+            },
+            {
+                "<leader>dg",
+                function()
+                    require("dap").goto_()
+                end,
+                desc = "Go to Line (No Execute)",
+            },
+            {
+                "<leader>di",
+                function()
+                    require("dap").step_into()
+                end,
+                desc = "Step Into",
+            },
+            {
+                "<leader>dj",
+                function()
+                    require("dap").down()
+                end,
+                desc = "Down",
+            },
+            {
+                "<leader>dk",
+                function()
+                    require("dap").up()
+                end,
+                desc = "Up",
+            },
+            {
+                "<leader>dl",
+                function()
+                    require("dap").run_last()
+                end,
+                desc = "Run Last",
+            },
+            {
+                "<leader>do",
+                function()
+                    require("dap").step_out()
+                end,
+                desc = "Step Out",
+            },
+            {
+                "<leader>dO",
+                function()
+                    require("dap").step_over()
+                end,
+                desc = "Step Over",
+            },
+            {
+                "<leader>dP",
+                function()
+                    require("dap").pause()
+                end,
+                desc = "Pause",
+            },
+            {
+                "<leader>dr",
+                function()
+                    require("dap").repl.toggle()
+                end,
+                desc = "Toggle REPL",
+            },
+            {
+                "<leader>ds",
+                function()
+                    require("dap").session()
+                end,
+                desc = "Session",
+            },
+            {
+                "<leader>dt",
+                function()
+                    require("dap").terminate()
+                end,
+                desc = "Terminate",
+            },
+            {
+                "<leader>dw",
+                function()
+                    require("dap.ui.widgets").hover()
+                end,
+                desc = "Widgets",
+            },
+        },
+        config = function()
+            local dap = require("dap")
+
+            vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+            vim.fn.sign_define("DapBreakpoint", { text = "" })
+            vim.fn.sign_define("DapBreakpointCondition", { text = "" })
+            vim.fn.sign_define("DapBreakpointRejected", { text = "" })
+            vim.fn.sign_define("DapLogPoint", { text = "" })
+            vim.fn.sign_define("DapStopped", { text = "" })
+
+            local wk = require("which-key")
+            wk.add({ "<leader>d", group = "Debugging" })
+
+            dap.adapters.codelldb = {
+                type = "executable",
+                command = "codelldb",
+            }
+        end,
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
+            "theHamsta/nvim-dap-virtual-text",
+        },
+        opts = {},
+        keys = {
+            {
+                "<leader>du",
+                function()
+                    require("dapui").toggle({})
+                end,
+                desc = "Toggle debug UI",
+            },
+            {
+                "<leader>de",
+                function()
+                    require("dapui").eval()
+                end,
+                desc = "Debug Eval",
+                mode = { "n", "v" },
+            },
+        },
+        config = function(_, opts)
+            local dap = require("dap")
+            local dapui = require("dapui")
+
+            dapui.setup(opts)
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+        end,
+    },
+}
